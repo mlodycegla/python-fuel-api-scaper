1fd18933159fc67a98e08372ef9f5c0deb73edff
Mateusz Koziel <mateyuszafgag@gmail.com>
2023-05-18 23:10:28 +0200

    change variable name for clarity

--- flaskr/scraper.py
+++ flaskr/scraper.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 # function that returns raw html text
 def get_raw_website(url):
     page = requests.get(url).text
@@ -11,12 +10,12 @@ def get_raw_website(url):
 
 # scrapes gas prices from given url and returns a list containing all information
 def europe_prices():
-    soup = get_raw_website(url="https://www.cargopedia.net/europe-fuel-prices")
+    html_code = get_raw_website(url="https://www.cargopedia.net/europe-fuel-prices")
 
     all_prices = []
 
     # searching the website for desired elements (in this case the country and prices of fuel)
-    country_table = soup.find("tbody")
+    country_table = html_code.find("tbody")
     country_list = country_table.find_all("tr")
     for country in country_list:
         name = country.find("td").text.strip()
