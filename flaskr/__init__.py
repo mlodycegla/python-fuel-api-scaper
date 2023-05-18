56099f46c71d37189963f1a67ea7e872e959486b
Mateusz Koziel <mateyuszafgag@gmail.com>
2023-05-18 23:22:34 +0200

    change how app is defined

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__, static_url_path="", static_folder="static", template_folder="templates"
+)
 
 
 @app.route("/")
