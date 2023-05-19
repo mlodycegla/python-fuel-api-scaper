9b6ed1025770e1c6b6cea3eb51727f51ccc11173
Mateusz Koziel <mateyuszafgag@gmail.com>
2023-05-19 14:57:08 +0200

    make tile on website clickable

---
+++ flaskr/static/js/main.js
@@ -0,0 +1,6 @@
+const europetile = document.getElementById("europeprices");
+const americantile = document.getElementById("americanprices");
+
+europetile.addEventListener("click", () => {
+  window.location.href = "/europe";
+});
