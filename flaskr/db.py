273c3eab8f747e7d52f0fa4863a690afc388d2e6
Mateusz Koziel <mateyuszafgag@gmail.com>
2023-05-19 14:56:35 +0200

    add function for pulling data from db

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,7 +4,7 @@ from scraper import europe_prices
 
 def init_db():
     if not os.path.exists("instance"):
-        os.makedir("instance")
+        os.makedirs("instance")
 
     # initializing database
     open("./instance/gasprices.db", "w+")
@@ -26,6 +26,31 @@ def init_db():
         connection.commit()
     except Exception as e:
         print(f"An error has occured: {e}")
+
+
+def get_europeprices_db(range=10, offset=0):
+    connection = sqlite3.connect("./instance/gasprices.db")
+    cursor = connection.cursor()
+
+    # makes the request to database and returns info about prices in a list
+    result = cursor.execute(
+        f"SELECT country, gasoline, diesel, lpg FROM europeprices LIMIT {range} OFFSET {offset}"
+    )
+    list = []
+    for row in result.fetchmany(range):
+        country = row[0]
+        gasprice = row[1]
+        dieselprice = row[2]
+        lpgprice = row[3]
+        list.append(
+            {
+                "country": country,
+                "gasolineprice": gasprice,
+                "dieselprice": dieselprice,
+                "lpgprice": lpgprice,
+            }
+        )
+    return list
 
 
 init_db()
