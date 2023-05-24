4e8546e91a94493cb18e837cd96adaf8203a55d4
Mateusz Koziel <mateyuszafgag@gmail.com>
2023-05-24 22:38:04 +0200

    make sql statements multiline

--- flaskr/db.py
+++ flaskr/db.py
@@ -23,7 +23,11 @@ def init_db():
     try:
         for country_prices in europe_gas_prices:
             cursor.execute(
-                f"INSERT INTO europeprices (COUNTRY, GASOLINE, DIESEL, LPG) values ('{country_prices['name']}', '{country_prices['95']}', '{country_prices['diesel']}', '{country_prices['lpg']}')"
+                f"""INSERT INTO europeprices (COUNTRY, GASOLINE, DIESEL, LPG) 
+                values ('{country_prices['name']}', 
+                '{country_prices['95']}', 
+                '{country_prices['diesel']}', 
+                '{country_prices['lpg']}')"""
             )
         connection.commit()
     except Exception as error:
@@ -36,7 +40,9 @@ def get_europeprices_db(range=10, offset=0):
 
     # makes the request to database and returns info about prices in a list
     result = cursor.execute(
-        f"SELECT country, gasoline, diesel, lpg FROM europeprices LIMIT {range} OFFSET {offset}"
+        f"""SELECT country, gasoline, diesel, lpg 
+        FROM europeprices 
+        LIMIT {range} OFFSET {offset}"""
     )
     list = []
     for row in result.fetchmany(range):
